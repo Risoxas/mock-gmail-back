a990585242bb242de0143abde6125c873c472ff9
Risoxas <davidag93@gmail.com>
2022-12-21 10:48:46 -0600

    initial setup

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Gmail mock')
+    .setDescription('Gmail clone application')
+    .setVersion('1.0')
+    .addTag('mail')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
